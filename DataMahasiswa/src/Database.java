e943ddf6cea2b9f0588d46996af6975ef4e42817
mdaffarh <daffarizmawan@upi.edu>
2025-03-28 16:02:37 +0700

    all files

---
+++ DataMahasiswa/src/Database.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+
+public class Database {
+    private Connection connection;
+
+    private Statement statement;
+
+    // constructor
+    public Database(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_mahasiswa", "root","");
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    select
+    public ResultSet selectQuery(String sql){
+        try {
+            statement.executeQuery(sql);
+            return statement.getResultSet();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+//    insert, update, delete
+    public int insertUpdateDeleteQuery(String sql){
+        try{
+            return statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    getter
+    public Statement getStatement(){
+        return statement;
+    }
+
+
+}
+
---
+++ DataMahasiswa/src/Menu.java
@@ -0,0 +1,287 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Menu extends JFrame{
+    public static void main(String[] args) {
+        // buat object window
+        Menu window = new Menu();
+
+        // atur ukuran window
+        window.setSize(480 + 100, 560 + 100);
+
+        // letakkan window di tengah layar
+        window.setLocationRelativeTo(null);
+
+        // isi window
+        window.setContentPane(window.mainPanel);
+
+        // ubah warna background
+        window.getContentPane().setBackground(Color.white);
+
+        // tampilkan window
+        window.setVisible(true);
+
+        // agar program ikut berhenti saat window diclose
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    // index baris yang diklik
+    private int selectedIndex = -1;
+    // list untuk menampung semua mahasiswa
+    private ArrayList<Mahasiswa> listMahasiswa;
+
+    private Database database;
+
+    private JPanel mainPanel;
+    private JTextField nimField;
+    private JTextField namaField;
+    private JTable mahasiswaTable;
+    private JButton addUpdateButton;
+    private JButton cancelButton;
+    private JComboBox jenisKelaminComboBox;
+    private JButton deleteButton;
+    private JLabel titleLabel;
+    private JLabel nimLabel;
+    private JLabel namaLabel;
+    private JLabel jenisKelaminLabel;
+    private JComboBox prodiComboBox;
+    private JLabel prodiLabel;
+
+    // constructor
+    public Menu() {
+        // inisialisasi listMahasiswa
+        listMahasiswa = new ArrayList<>();
+
+        database = new Database();
+
+        // isi tabel mahasiswa
+        mahasiswaTable.setModel(setTable());
+
+
+        // ubah styling title
+        titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD, 20f));
+
+        // atur isi combo box
+        String[] jenisKelaminData = {"", "Laki-laki", "Perempuan"};
+        jenisKelaminComboBox.setModel(new DefaultComboBoxModel(jenisKelaminData));
+
+        // atur isi combo box prodi
+        String[] prodiData = {"", "Ilmu Komputer", "Pendidikan Ilmu Komputer"};
+        prodiComboBox.setModel(new DefaultComboBoxModel(prodiData));
+
+        // sembunyikan button delete
+        deleteButton.setVisible(false);
+
+        // saat tombol add/update ditekan
+        addUpdateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(selectedIndex == -1){
+                    insertData();
+                }else{
+                    updateData();;
+                }
+            }
+        });
+
+        // saat tombol delete ditekan
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(selectedIndex >= 0 ){
+                    deleteData();
+                }
+            }
+        });
+        // saat tombol cancel ditekan
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearForm();
+            }
+        });
+        // saat salah satu baris tabel ditekan
+        mahasiswaTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // ubah selectedIndex menjadi baris tabel yang diklik
+                selectedIndex = mahasiswaTable.getSelectedRow();
+
+                // simpan value textfield dan combo box
+                String selectedNim = mahasiswaTable.getModel().getValueAt(selectedIndex, 1).toString();
+                String selectedNama = mahasiswaTable.getModel().getValueAt(selectedIndex, 2).toString();
+                String selectedJenisKelamin = mahasiswaTable.getModel().getValueAt(selectedIndex, 3).toString();
+                String selectedProdi = mahasiswaTable.getModel().getValueAt(selectedIndex, 4).toString();
+
+                // ubah isi textfield dan combo box
+                nimField.setText(selectedNim);
+                namaField.setText(selectedNama);
+                jenisKelaminComboBox.setSelectedItem(selectedJenisKelamin);
+                prodiComboBox.setSelectedItem(selectedProdi);
+
+                // ubah button "Add" menjadi "Update"
+                addUpdateButton.setText("Update");
+
+                // tampilkan button delete
+                deleteButton.setVisible(true);
+            }
+        });
+    }
+
+    public final DefaultTableModel setTable() {
+        // tentukan kolom tabel
+        Object[] column = {"No", "NIM", "Nama", "Jenis Kelamin", "Prodi"};
+
+        // buat objek tabel dengan kolom yang sudah dibuat
+        DefaultTableModel temp = new DefaultTableModel(null, column);
+
+        try {
+            ResultSet resultSet = database.selectQuery("SELECT * FROM mahasiswa");
+
+            int i = 0;
+            while (resultSet.next()){
+                Object[] row = new Object[5];
+
+                row[0] = i + 1;
+                row[1] = resultSet.getString("nim");
+                row[2] = resultSet.getString("nama");
+                row[3] = resultSet.getString("jenis_kelamin");
+                row[4] = resultSet.getString("prodi");
+
+                temp.addRow(row);
+                i++;
+            }
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return temp; // return juga harus diganti
+    }
+
+    public void insertData() {
+        // ambil value dari textfield dan combobox
+        String nim = nimField.getText().trim();
+        String nama = namaField.getText().trim();
+        String jenisKelamin = jenisKelaminComboBox.getSelectedItem().toString();
+        String prodi = prodiComboBox.getSelectedItem().toString();
+
+        // validasi input
+        if (nim.isEmpty() || nama.isEmpty() || jenisKelamin == null || prodi == null) {
+            JOptionPane.showMessageDialog(null, "Semua kolom harus diisi!", "Error", JOptionPane.ERROR_MESSAGE);
+            return; // hentikan proses
+        }
+
+        ResultSet resultSet = database.selectQuery("SELECT COUNT(*) FROM mahasiswa WHERE nim = '" + nim + "';");
+
+        try {
+            if (resultSet.next() && resultSet.getInt(1) > 0) {
+                // jika NIM sudah ada, tampilkan pesan error
+                JOptionPane.showMessageDialog(null, "NIM sudah ada!");
+                return;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        // tambahkan data ke dalam list
+        String sql = "INSERT INTO mahasiswa VALUES (null, '"+ nim +"' ,'"+nama+"', '"+jenisKelamin+"', '"+prodi+"');";
+        database.insertUpdateDeleteQuery(sql);
+
+        // update tabel
+        mahasiswaTable.setModel(setTable());
+
+        // bersihkan form
+        clearForm();
+
+        // feedback
+        System.out.println("Insert Berhasil");
+        JOptionPane.showMessageDialog(null, "Data berhasil ditambahkan!");
+
+    }
+
+    public void updateData() {
+        // ambil data dari form
+        String nim = nimField.getText();
+        String nama = namaField.getText();
+        String jenisKelamin = jenisKelaminComboBox.getSelectedItem().toString();
+        String prodi = prodiComboBox.getSelectedItem().toString();
+
+        // validasi input
+        if (nim.isEmpty() || nama.isEmpty() || jenisKelamin == null || prodi == null) {
+            JOptionPane.showMessageDialog(null, "Semua kolom harus diisi!", "Error", JOptionPane.ERROR_MESSAGE);
+            return; // hentikan proses
+        }
+
+        // ubah data mahasiswa di
+        String sql = "UPDATE mahasiswa SET nama = '" + nama +
+                "', jenis_kelamin = '" + jenisKelamin +
+                "', prodi = '" + prodi +
+                "' WHERE nim = '" + nim + "';";
+        database.insertUpdateDeleteQuery(sql);
+
+        // update tabel
+        mahasiswaTable.setModel(setTable());
+
+        // bersihkan form
+        clearForm();
+
+        // feedback
+        System.out.println("Update Berhasil");
+        JOptionPane.showMessageDialog(null, "Data berhasil diubah!");
+    }
+
+    public void deleteData() {
+        int selectedRow = mahasiswaTable.getSelectedRow(); // ambil baris yang dipilih
+
+        // ambil NIM
+        String nim = mahasiswaTable.getValueAt(selectedRow, 1).toString();
+
+        // konfirmasi
+        int confirm = JOptionPane.showConfirmDialog(null, "Apakah Anda yakin ingin menghapus data ini?", "Konfirmasi", JOptionPane.YES_NO_OPTION);
+        if (confirm != JOptionPane.YES_OPTION) {
+            return; // batal
+        }
+
+        // query untuk menghapus data
+        String sql = "DELETE FROM mahasiswa WHERE nim = '" + nim + "';";
+        database.insertUpdateDeleteQuery(sql);
+
+        // update tabel setelah penghapusan
+        mahasiswaTable.setModel(setTable());
+
+        // bersihkan form
+        clearForm();
+
+        // feedback
+        System.out.println("Delete berhasil!");
+        JOptionPane.showMessageDialog(null, "Data berhasil dihapus!");
+    }
+
+
+    public void clearForm() {
+        // kosongkan semua texfield dan combo box
+        nimField.setText("");
+        namaField.setText("");
+        jenisKelaminComboBox.setSelectedItem("");
+        prodiComboBox.setSelectedItem("");
+
+        // ubah button "Update" menjadi "Add"
+        addUpdateButton.setText("Add");
+
+        // sembunyikan button delete
+        deleteButton.setVisible(false);
+
+        // ubah selectedIndex menjadi -1 (tidak ada baris yang dipilih)
+        selectedIndex = -1;
+
+    }
+}
